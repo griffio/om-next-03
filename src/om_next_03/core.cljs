2cb0145e1343bdfdf6b6c9c8cd92d52ac8648eb4
griffio <griffio@users.noreply.github.com>
2015-11-14 09:42:40 +0000

    updates to ui
    change names and events

--- src/om_next_03/core.cljs
+++ src/om_next_03/core.cljs
@@ -19,7 +19,7 @@
 (defn remove-episode [episodes ep]
   (filter (fn [it] (not= it ep)) episodes))
 
-(defn move-episode [episodes from to]
+(defn drag-episode [episodes from to]
   (let [val (episodes from) parts (split-at to (remove-episode episodes val)) part1 (first parts) part2 (second parts)]
     (vec (concat part1 [val] part2))))
 
@@ -45,19 +45,22 @@
                  (dom/li nil
                          (dom/div #js {:className   "js-dragging"
                                        :draggable   true
-                                       :onDragStart (fn [e] (let [datr (.-dataTransfer e) {:keys [drag-start index]} (om/get-computed this)]
+                                       :onDragStart (fn [e] (let [datr (.-dataTransfer e) {:keys [drag-start]} (om/get-computed this)]
                                                               (set! (.-effectAllowed datr) "move")
                                                               (.setData datr "text/html" "")
-                                                              (drag-start e index)))
-                                       :onDragEnd   (fn [e] (let [ident (om/get-ident this)]))
-                                       :onDragOver  (fn [e] (let [ident (om/get-ident this)]
+                                                              (drag-start e)))
+                                       :onDragEnd   (fn [e] (let [{:keys [drag-end]} (om/get-computed this)]
+                                                              (drag-end e)))
+                                       :onDragOver  (fn [e] (let [{:keys [drag-over]} (om/get-computed this)]
                                                               (.preventDefault e)
-                                                              (-> e .-dataTransfer .-dropEffect (set! "move"))))
-                                       :onDrop      (fn [e] (let [{:keys [drag-drop index]} (om/get-computed this)]
-                                                              (drag-drop e index)))}
+                                                              (-> e .-dataTransfer .-dropEffect (set! "move"))
+                                                              (drag-over e)))
+                                       :onDrop      (fn [e] (let [{:keys [drag-drop]} (om/get-computed this)]
+                                                              (.preventDefault e)
+                                                              (drag-drop e)))}
                                   (dom/h3 nil (str episode ". " title))
-                                  (dom/a #js {:href (str "http://www.imdb.com/title/" imdbID)} "imdb")
-                                  (dom/label nil "rating:") (dom/span nil imdbRating))))))
+                                  (dom/a #js {:href (str "http://www.imdb.com/title/" imdbID)} "imdb rating:")
+                                  (dom/label nil imdbRating))))))
 
 (def episode-ui (om/factory Episode {:keyfn :episode}))
 
@@ -69,17 +72,15 @@
        (drag-start [this e index]
                    (om/update-state! this assoc :dragged-index index))
 
-       (drag-end [this e]
+       (drag-end [this e index]
                  (om/update-state! this dissoc :dragged-index))
 
-       (drag-over [this e]
-                  (.preventDefault e))
+       (drag-over [this e index])
 
        (drag-drop [this e index]
-                  (let [from-index (:dragged-index (om/get-state this))
-                        to-index index]
-                    (.preventDefault e)
-                    (om/transact! this `[(episodes/move {:move [~from-index ~to-index]}) :episodes]))) ;; `[(query) read)] mutate specification
+                  (let [from-idx (:dragged-index (om/get-state this))
+                        to-idx index]
+                    (om/transact! this `[(episodes/drag {:from ~from-idx :to ~to-idx})]))) ;; `[(query) read)] - https://github.com/omcljs/om/wiki/Om-Next-FAQ
        (render [this]
                (let [{:keys [episodes]} (om/props this)]
                  (dom/div nil
@@ -87,11 +88,10 @@
                                   (map-indexed
                                     (fn [idx ep]
                                       (episode-ui (om/computed ep
-                                                               {:index      idx ;; use the index to help with drag drop
-                                                                :drag-start (fn [e k] (.drag-start this e k)) ;; could use idx param here
-                                                                :drag-end   #(.drag-end this %)
-                                                                :drag-over  #(.drag-over this %)
-                                                                :drag-drop  (fn [e k] (.drag-drop this e k))})))
+                                                               {:drag-start (fn [e] (.drag-start this e idx)) ;; associate the episode ordinal index with each callback
+                                                                :drag-end   (fn [e] (.drag-end this idx))
+                                                                :drag-over  (fn [e] (.drag-over this idx))
+                                                                :drag-drop  (fn [e] (.drag-drop this e idx))})))
                                     episodes))))))
 
 (defmulti reading om/dispatch)
@@ -103,18 +103,13 @@
       {:value (get st key)}                                 ;; loads data from app state
       {:remote-episodes true})))                            ;; loads data from external file /episodes.json
 
-(defmethod reading :episode/dragged
-  [{:keys [state]} key _]
-  (let [st @state]
-    {:value (get st key)}))
-
 (defmulti mutating om/dispatch)
 
-(defmethod mutating 'episodes/move
-  [{:keys [state]} _ {:keys [move]}]
+(defmethod mutating 'episodes/drag
+  [{:keys [state]} _ {:keys [from to]}]
   {:value {:keys [:episodes]}
    :action
-          (fn [] (swap! state update :episodes move-episode (first move) (second move)))})
+          (fn [] (swap! state update :episodes drag-episode from to))})
 
 (def reconciler
   (om/reconciler
@@ -122,7 +117,7 @@
      :normalize false
      :remotes   [:remote-episodes]                          ;; vector of remotes
      :parser    (om/parser {:read reading :mutate mutating})
-     :send      (reconciler-send "/episodes.json")}))
+     :send      (reconciler-send "/episodes2.json")}))
 
 (om/add-root! reconciler Episodes (gdom/getElement "ui"))
 
