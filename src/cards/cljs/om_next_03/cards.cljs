60468b1bec8cfedb46d10d879d00fff4260836b1
griffio <griffio@users.noreply.github.com>
2015-12-02 20:08:43 +0000

    dc/deftest

--- src/cards/cljs/om_next_03/cards.cljs
+++ src/cards/cljs/om_next_03/cards.cljs
@@ -5,7 +5,17 @@
   (:require
     [om-next-03.core :refer [drag-episode]]))
 
-(dc/deftest must-drag-episode-from-first-to-last
+(dc/deftest dragging-episode-from-first-to-last-position
             "returns a vector with the episodes in new ordering"
             (let [ eps [{:ep 1} {:ep 2} {:ep 3}]]
               (is (= (drag-episode eps 0 2) [{:ep 2} {:ep 3} {:ep 1}]))))
+
+(dc/deftest dragging-episode-from-last-to-first-position
+            "returns a vector with the episodes in new ordering"
+            (let [ eps [{:ep 1} {:ep 2} {:ep 3}]]
+              (is (= (drag-episode eps 2 0) [{:ep 3} {:ep 1} {:ep 2}]))))
+
+(dc/deftest single-episode
+            "returns a vector with single episode"
+            (let [ eps [{:ep 1}]]
+              (is (= (drag-episode eps 0 0) eps))))
