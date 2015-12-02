a3ee4b47444332648a3666e8de49d9f6869a21c0
griffio <griffio@users.noreply.github.com>
2015-12-02 12:59:28 +0000

    deftest

--- src/cards/cljs/om_next_03/cards.cljs
+++ src/cards/cljs/om_next_03/cards.cljs
@@ -1,7 +1,11 @@
 (ns om-next-03.cards
   (:require-macros
-    [devcards.core :as dc :refer [defcard defcard-doc noframe-doc deftest dom-node]]))
+    [cljs.test :refer [is]]
+    [devcards.core :as dc :refer [defcard defcard-doc noframe-doc deftest dom-node]])
+  (:require
+    [om-next-03.core :refer [drag-episode]]))
 
-(defcard {}               ; main obj
-         {}               ; initial data
-         {:heading true})
+(dc/deftest must-drag-episode-from-first-to-last
+            "returns a vector with the episodes in new ordering"
+            (let [ eps [{:ep 1} {:ep 2} {:ep 3}]]
+              (is (= (drag-episode eps 0 2) [{:ep 2} {:ep 3} {:ep 1}]))))
