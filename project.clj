b09f1f0812cc2073433a82c9784009e59a341423
griffi⊙ <griffio@users.noreply.github.com>
2016-08-01 16:59:15 +0100

    1.0.0-alpha41

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [datascript "0.13.3"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "1.0.0-alpha40"]
+                 [org.omcljs/om "1.0.0-alpha41"]
                  [figwheel-sidecar "0.5.4-3" :scope "test"]]
 
   :source-paths ["src/main/cljs" "src/cards/cljs"]
