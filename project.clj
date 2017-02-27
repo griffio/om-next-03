bdf5a1318a2438f2dfa92db6d47756e8321fb33b
griff і⊙ <griffio@users.noreply.github.com>
2017-02-27 21:54:36 +0000

    updates to 1.0.0-alpha48

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject om-tutorial "0.1.0-SNAPSHOT"
   :description "A Tutorial for Om 1.0.0 (next)"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.89" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.494" :scope "provided"]
                  [devcards "0.2.1-6" :exclusions [org.omcljs/om]]
                  [datascript "0.13.3"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "1.0.0-alpha47"]
+                 [org.omcljs/om "1.0.0-alpha48"]
                  [figwheel-sidecar "0.5.7" :scope "test"]]
 
   :source-paths ["src/main/cljs" "src/cards/cljs"]
