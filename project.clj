865d9c91ac17c582f27024fc40eedb04e4a53387
griffio <griffio@users.noreply.github.com>
2017-06-10 22:08:57 +0100

    1.0.0-beta1
    updates to 1.0.0-beta1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject om-tutorial "0.1.0-SNAPSHOT"
   :description "A Tutorial for Om 1.0.0 (next)"
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.521" :scope "provided"]
-                 [devcards "0.2.1-6" :exclusions [org.omcljs/om]]
+                 [org.clojure/clojurescript "1.9.562" :scope "provided"]
+                 [devcards "0.2.3" :exclusions [org.omcljs/om]]
                  [datascript "0.13.3"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
